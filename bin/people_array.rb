2cfdf43655f5e036241e8d661a31f12d26f9aa34
“sstone72389” <sstone72389@gmail.com>
2017-04-19 14:04:21 -0400

    Completes ruby array method training
    completes required training

--- bin/people_array.rb
+++ bin/people_array.rb
@@ -5,3 +5,22 @@ require_relative '../lib/people.rb'
 people = People.new.people
 
 p people.count
+
+# Selects all females from list
+
+female = people.select { |person| person.gender == 'f' }
+
+# displays the count of females
+p female.count
+
+# selects by age and counts those younger than 27
+
+age_limit = people.select { |person| person.age < 27 }
+
+p age_limit.count
+
+# selects by age and counts those older than 27
+
+older_than = people.select { |person| person.age > 27 }
+
+p older_than.count
--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -2,4 +2,52 @@
 
 fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-p fibs
+# calculates the product of elements of fibs (excluding zero).
+# whole thing shorthand
+fibs.select(&:positive?).reduce(1, :*)
+# whole thing written out
+fibs.select { |num|  num.positive? }.reduce(1, :*)
+
+# selects only positive numbers from array
+fibs.select! { |num|  num.positive? }
+
+# shorthand for product
+fibs.reduce(1, :*)
+
+# Method written out for above for reduce (inject is the same)
+fibs.inject(1) { |product, n| product * n }
+
+
+
+# calculates sum of odd elements
+# whole thing shorthand
+fibs.select(&:odd?).reduce(1, :+)
+# whole thing written out
+fibs.select { |num|  num.odd? }.reduce(0, :+)
+
+# calculates sum of odd elements
+# fisrt selects them
+fibs.select! { |num|  num.odd? }
+
+# then sums them (shorthand)
+fibs.reduce(0, :+)
+
+# entire block showing the aboves functionality
+fibs.reduce(0) { |acc, num| acc + num }
+
+
+
+
+# calculates product of even elements
+# whole thing
+fibs.select { |num|  num.even? and num.positive?}.reduce(1, :*)
+
+# calculates product of even elements
+# fisrt selects them
+fibs.select! { |num|  num.even? and num.positive?}
+
+#shorthand
+fibs.reduce(1, :*)
+
+# Method written out for above for reduce (inject is the same)
+fibs.inject(1) { |product, n| product * n }
